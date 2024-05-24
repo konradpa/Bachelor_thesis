efa305446211a6c1995b078676ba1f36b9415f95
konrad <test@dhcp-172-21-65-113.wlan.uni-hamburg.de.local>
2024-05-24 15:30:38 +0200

    add info data + WPT order to ETQ data

--- analyses.R
+++ analyses.R
@@ -2,6 +2,7 @@ install.packages("readr")
 install.packages("dplyr")
 install.packages("janitor")
 install.packages("ggplot2")
+install.packages("readxl")
 
 
 # Install and load necessary packages
@@ -9,25 +10,47 @@ library(readr)
 library(dplyr)
 library(janitor)
 library(ggplot2)
+library(readxl)
 source("functions.R")
 
-# Read the data from the file
+# Read the ETQ data from the file
 ETQ_data <- read_delim(ETQ_path, delim = ";", locale = locale(encoding = "UTF-8"))
 
-# Clean the column names
+#Read the particpant information sheet
+info_data <- read_excel(info_path)
+
+# Clean the ETQ column names
 ETQ_data <- ETQ_data %>%
   clean_names()
+
+#add card order to ETQ data
+ETQ_data <- ETQ_data %>%
+  rename(VPN = id_antwort_id)
+
+ETQ_data <- ETQ_data %>%
+  mutate(VPN = as.character(VPN))
+
+participant_data <- info_data %>%
+  mutate(VPN = as.character(VPN))
+
+ETQ_data <- ETQ_data %>%
+  left_join(info_data %>% select(VPN, "WPT _randomCardOrderMap"), by = "VPN")
+
 
 # Analyze the data using the function
 ETQ_data <- analyze_etq_data(ETQ_data)
 
 # View the data with the new overall_score column as the first column
-print(head(ETQ_data))
+print(colnames(ETQ_data))
 
 mean(ETQ_data$overall_score)
 
 # Call the function to visualize the overall scores
 visualize_overall_scores(ETQ_data)
+
+ETQ_data$id_antwort_id
+
+
 
 
 
